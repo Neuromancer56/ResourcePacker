7911806557fe9145a59b7734c4d0426a1e9b9b5f
Neuromancer56 <neuromancer56@gmail.com>
2014-09-21 09:21:10 -0500

    initial commit

---
+++ resourcepacker/init.lua
@@ -0,0 +1,82 @@
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("stairs:stair_brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("stairs:slab_brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:clay", {
+	tiles = {"clay.png"},
+})
+
+minetest.override_item("default:coalblock", {
+	tiles = {"coal_block.png"},
+})
+minetest.override_item("default:cobble", {
+	tiles = {"cobblestone.png"},
+})
+minetest.override_item("stairs:stair_cobble", {
+	tiles = {"cobblestone.png"},
+})
+minetest.override_item("stairs:slab_cobble", {
+	tiles = {"cobblestone.png"},
+})
+minetest.override_item("default:mossycobble", {
+	tiles = {"cobblestone_mossy.png"},
+})
+minetest.override_item("default:dry_shrub", {
+	tiles = {"deadbush.png"},
+})
+minetest.override_item("default:diamondblock", {
+	tiles = {"diamond_block.png"},
+})
+
+minetest.override_item("default:dirt", {
+	tiles = {"dirt.png"},
+})
+minetest.override_item("doors:door_steel", {
+	tiles_bottom = {"door_iron_lower.png", "door_grey.png"},
+	tiles_top = {"door_iron_upper.png", "door_grey.png"},
+})
+minetest.override_item("doors:door_wood", {
+	tiles_bottom = {"door_wood_lower.png", "door_brown.png"},
+	tiles_top = {"door_wood_upper.png", "door_brown.png"},
+})
+minetest.override_item("farming:soil", {
+	tiles = {"farmland_dry.png"},
+})
+minetest.override_item("farming:soil_wet", {
+	tiles = {"farmland_wet.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+minetest.override_item("default:brick", {
+	tiles = {"brick.png"},
+})
+
+minetest.override_item("default:stone", {
+	tiles = {"stone.png"},
+})
+minetest.override_item("stairs:stair_stone", {
+	tiles = {"stone.png"},
+})
+minetest.override_item("stairs:slab_stone", {
+	tiles = {"stone.png"},
+})
+
+
