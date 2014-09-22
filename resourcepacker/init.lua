7bbbc66191cd4794452ae665e25847c82cb689cb
Neuromancer56 <neuromancer56@gmail.com>
2014-09-21 21:11:09 -0500

    check for files existence
    check for files existence

--- resourcepacker/init.lua
+++ resourcepacker/init.lua
@@ -1,41 +1,35 @@
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("stairs:stair_brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("stairs:slab_brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:clay", {
-	tiles = {"clay.png"},
-})
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+function mod_path() 
+	local dir = minetest.get_modpath(minetest.get_current_modname())
+	return dir
+end 
 
-minetest.override_item("default:coalblock", {
-	tiles = {"coal_block.png"},
-})
-minetest.override_item("default:cobble", {
-	tiles = {"cobblestone.png"},
-})
-minetest.override_item("stairs:stair_cobble", {
-	tiles = {"cobblestone.png"},
-})
-minetest.override_item("stairs:slab_cobble", {
-	tiles = {"cobblestone.png"},
-})
-minetest.override_item("default:mossycobble", {
-	tiles = {"cobblestone_mossy.png"},
-})
-minetest.override_item("default:dry_shrub", {
-	tiles = {"deadbush.png"},
-})
-minetest.override_item("default:diamondblock", {
-	tiles = {"diamond_block.png"},
-})
+function map_resource(node, file)
+	if file_exists(mod_path().."\\textures\\"..file) then
+		minetest.override_item(node, {
+	tiles = {file},
+	})
+	end
+	print(mod_path())
+	minetest.chat_send_all(mod_path())
+end
 
-minetest.override_item("default:dirt", {
-	tiles = {"dirt.png"},
-})
+map_resource( "default:brick", "brick.png")
+map_resource( "stairs:stair_brick", "brick.png")
+map_resource("stairs:slab_brick","brick.png")
+map_resource("default:clay","clay.png")
+map_resource("default:coalblock","coal_block.png")
+map_resource("default:cobble", "cobblestone.png")
+map_resource("stairs:stair_cobble","cobblestone.png")
+map_resource("stairs:slab_cobble","cobblestone.png")
+map_resource("default:mossycobble","cobblestone_mossy.png")
+map_resource("default:dry_shrub","deadbush.png")
+map_resource("default:diamondblock","diamond_block.png")
+map_resource("default:dirt","dirt.png")
+
 minetest.override_item("doors:door_steel", {
 	tiles_bottom = {"door_iron_lower.png", "door_grey.png"},
 	tiles_top = {"door_iron_upper.png", "door_grey.png"},
@@ -44,39 +38,16 @@ minetest.override_item("doors:door_wood", {
 	tiles_bottom = {"door_wood_lower.png", "door_brown.png"},
 	tiles_top = {"door_wood_upper.png", "door_brown.png"},
 })
-minetest.override_item("farming:soil", {
-	tiles = {"farmland_dry.png"},
-})
-minetest.override_item("farming:soil_wet", {
-	tiles = {"farmland_wet.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
-minetest.override_item("default:brick", {
-	tiles = {"brick.png"},
-})
 
-minetest.override_item("default:stone", {
-	tiles = {"stone.png"},
-})
-minetest.override_item("stairs:stair_stone", {
-	tiles = {"stone.png"},
-})
-minetest.override_item("stairs:slab_stone", {
-	tiles = {"stone.png"},
-})
-
+map_resource("farming:soil","farmland_dry.png")
+map_resource("farming:soil_wet","farmland_wet.png")
+map_resource("flowers:viola","flower_blue_orchid.png")
+map_resource("flowers:dandelion_yellow","flower_dandelion.png")
+map_resource("flowers:rose","flower_rose.png")
+map_resource("flowers:dandelion_white","flower_oxeye_daisy.png")
+map_resource("flowers:geranium","flower_houstonia.png")
+map_resource("flowers:tulip","flower_tulip_orange.png")
+map_resource("default:stone","stone.png")
+map_resource("stairs:stair_stone","stone.png")
+map_resource("stairs:slab_stone","stone.png")
 
--- resourcepacker/readme.txt
+++ resourcepacker/readme.txt
@@ -1,4 +1,4 @@
-Usuage:
+Usage:
 Install the resourcepacker mod like any other Minetest mod.
 Copy the contents of the assets\minecraft\textures\blocks folder into the resourcePacker's textures folder.
 You are good to go.  :)
