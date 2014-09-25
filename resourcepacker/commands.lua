d9f0d457bae89838a15e8b4d625af435cbe6f769
Neuromancer56 <neuromancer56@gmail.com>
2014-09-24 20:37:24 -0500

    renamed a bunch of files

--- resourcepacker/commands.lua
+++ resourcepacker/commands.lua
@@ -3,6 +3,24 @@ minetest.register_chatcommand("rpren", {
 	description = "Rename all the resource pack .png files to standard Minetest filenames. (only needs to be run once per pack)",
 	func = function(name, param)
 		rename_file("brick.png", "default_brick.png");
+		rename_file("clay.png", "default_clay.png");
+		rename_file("coal_block.png", "default_coalblock.png");
+		rename_file("cobblestone.png", "default_cobble.png");
+		rename_file("cobblestone_mossy.png", "default_mossycobble.png");
+		rename_file("deadbush.png", "dry_shrub.png");
+		rename_file("diamond_block.png", "default_diamondblock.png");
+		rename_file("dirt.png", "default_dirt.png");
+		rename_file("farmland_wet.png", "farming_soil_wet.png");
+		rename_file("flower_blue_orchid.png", "flowers_viola.png");
+		rename_file("flower_dandelion.png", "flowers_dandelion_yellow.png");
+		rename_file("flower_rose.png", "flowers_rose.png");
+		rename_file("flower_oxeye_daisy.png", "flowers_dandelion_white.png");
+		rename_file("flower_houstonia.png", "flowers_geranium.png");
+		rename_file("flower_tulip_orange.png", "flowers_tulip.png");
+		
+		
+		
+		rename_file("stone.png", "default_stone.png");		
         return true
     end,
 })
@@ -16,19 +34,6 @@ minetest.register_chatcommand("rpget", {
 })
 
 --[[
-map_resource( "default:brick", "brick.png")
-map_resource( "stairs:stair_brick", "brick.png")
-map_resource("stairs:slab_brick","brick.png")
-map_resource("default:clay","clay.png")
-map_resource("default:coalblock","coal_block.png")
-map_resource("default:cobble", "cobblestone.png")
-map_resource("stairs:stair_cobble","cobblestone.png")
-map_resource("stairs:slab_cobble","cobblestone.png")
-map_resource("default:mossycobble","cobblestone_mossy.png")
-map_resource("default:dry_shrub","deadbush.png")
-map_resource("default:diamondblock","diamond_block.png")
-map_resource("default:dirt","dirt.png")
-
 minetest.override_item("doors:door_steel", {
 	tiles_bottom = {"door_iron_lower.png", "door_grey.png"},
 	tiles_top = {"door_iron_upper.png", "door_grey.png"},
@@ -37,7 +42,6 @@ minetest.override_item("doors:door_wood", {
 	tiles_bottom = {"door_wood_lower.png", "door_brown.png"},
 	tiles_top = {"door_wood_upper.png", "door_brown.png"},
 })
-
 map_resource("farming:soil","farmland_dry.png")
 map_resource("farming:soil_wet","farmland_wet.png")
 map_resource("flowers:viola","flower_blue_orchid.png")
