40e0813b9bdc21a435f7fb372abc8563a883e7f4
Neuromancer56 <neuromancer56@gmail.com>
2014-09-24 20:13:44 -0500

    got rename working

--- resourcepacker/commands.lua
+++ resourcepacker/commands.lua
@@ -2,6 +2,7 @@ minetest.register_chatcommand("rpren", {
 	params = "<text>",
 	description = "Rename all the resource pack .png files to standard Minetest filenames. (only needs to be run once per pack)",
 	func = function(name, param)
+		rename_file("brick.png", "default_brick.png");
         return true
     end,
 })
@@ -13,3 +14,39 @@ minetest.register_chatcommand("rpget", {
         return true
     end,
 })
+
+--[[
+map_resource( "default:brick", "brick.png")
+map_resource( "stairs:stair_brick", "brick.png")
+map_resource("stairs:slab_brick","brick.png")
+map_resource("default:clay","clay.png")
+map_resource("default:coalblock","coal_block.png")
+map_resource("default:cobble", "cobblestone.png")
+map_resource("stairs:stair_cobble","cobblestone.png")
+map_resource("stairs:slab_cobble","cobblestone.png")
+map_resource("default:mossycobble","cobblestone_mossy.png")
+map_resource("default:dry_shrub","deadbush.png")
+map_resource("default:diamondblock","diamond_block.png")
+map_resource("default:dirt","dirt.png")
+
+minetest.override_item("doors:door_steel", {
+	tiles_bottom = {"door_iron_lower.png", "door_grey.png"},
+	tiles_top = {"door_iron_upper.png", "door_grey.png"},
+})
+minetest.override_item("doors:door_wood", {
+	tiles_bottom = {"door_wood_lower.png", "door_brown.png"},
+	tiles_top = {"door_wood_upper.png", "door_brown.png"},
+})
+
+map_resource("farming:soil","farmland_dry.png")
+map_resource("farming:soil_wet","farmland_wet.png")
+map_resource("flowers:viola","flower_blue_orchid.png")
+map_resource("flowers:dandelion_yellow","flower_dandelion.png")
+map_resource("flowers:rose","flower_rose.png")
+map_resource("flowers:dandelion_white","flower_oxeye_daisy.png")
+map_resource("flowers:geranium","flower_houstonia.png")
+map_resource("flowers:tulip","flower_tulip_orange.png")
+map_resource("default:stone","stone.png")
+map_resource("stairs:stair_stone","stone.png")
+map_resource("stairs:slab_stone","stone.png")
+--]]
--- resourcepacker/init.lua
+++ resourcepacker/init.lua
@@ -1,54 +1,29 @@
 dofile(minetest.get_modpath("resourcepacker").."/commands.lua")
+--dofile(minetest.get_modpath(minetest.get_current_modname()).."/commands.lua")
 
 function file_exists(name)
    local f=io.open(name,"r")
    if f~=nil then io.close(f) return true else return false end
 end
 function mod_path()
-	local dir = minetest.get_modpath(minetest.get_current_modname())
+	--local dir = minetest.get_modpath(minetest.get_current_modname())
+	local dir = minetest.get_modpath("resourcepacker")
 	return dir
 end
 
+--[[
 function map_resource(node, file)
 	if file_exists(mod_path().."\\textures\\"..file) then
 		minetest.override_item(node, {
 	tiles = {file},
 	})
 	end
-	print(mod_path())
-	minetest.chat_send_all(mod_path())
+end
+--]]
+
+function rename_file(source, destination)
+	if file_exists(mod_path().."\\textures\\"..source) then
+	os.rename(mod_path().."\\textures\\"..source, mod_path().."\\textures\\"..destination)
+	end
 end
 
-map_resource( "default:brick", "brick.png")
-map_resource( "stairs:stair_brick", "brick.png")
-map_resource("stairs:slab_brick","brick.png")
-map_resource("default:clay","clay.png")
-map_resource("default:coalblock","coal_block.png")
-map_resource("default:cobble", "cobblestone.png")
-map_resource("stairs:stair_cobble","cobblestone.png")
-map_resource("stairs:slab_cobble","cobblestone.png")
-map_resource("default:mossycobble","cobblestone_mossy.png")
-map_resource("default:dry_shrub","deadbush.png")
-map_resource("default:diamondblock","diamond_block.png")
-map_resource("default:dirt","dirt.png")
-
-minetest.override_item("doors:door_steel", {
-	tiles_bottom = {"door_iron_lower.png", "door_grey.png"},
-	tiles_top = {"door_iron_upper.png", "door_grey.png"},
-})
-minetest.override_item("doors:door_wood", {
-	tiles_bottom = {"door_wood_lower.png", "door_brown.png"},
-	tiles_top = {"door_wood_upper.png", "door_brown.png"},
-})
-
-map_resource("farming:soil","farmland_dry.png")
-map_resource("farming:soil_wet","farmland_wet.png")
-map_resource("flowers:viola","flower_blue_orchid.png")
-map_resource("flowers:dandelion_yellow","flower_dandelion.png")
-map_resource("flowers:rose","flower_rose.png")
-map_resource("flowers:dandelion_white","flower_oxeye_daisy.png")
-map_resource("flowers:geranium","flower_houstonia.png")
-map_resource("flowers:tulip","flower_tulip_orange.png")
-map_resource("default:stone","stone.png")
-map_resource("stairs:stair_stone","stone.png")
-map_resource("stairs:slab_stone","stone.png")
